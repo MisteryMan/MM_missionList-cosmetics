e21551b3bd52467802231dd1b469af577d55c7db
MisteryMan <MisteryMan@users.noreply.github.com>
2020-07-14 16:14:09 +0200

    Initial commit

---
+++ MM_missionList-cosmetics.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         MM_missionList-cosmetics
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.missionchief.com/
+// @match        https://meldkamerspel.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $("<style type='text/css' id='MM_missionList-cosmetics'>" +
+      ".mission_panel_red > .panel-heading { background-color: #c9302c5e !important; }" +
+      ".mission_panel_red > .panel-body { background-color: #c9302c5e !important; }" +
+      ".mission_panel_yellow > .panel-heading { background-color: #f1f16f !important; }" +
+      ".mission_panel_green > .panel-heading { background-color: lightgreen!important; }" +
+      ".panel-default>.panel-heading { background-image: none!important; }" +
+      "#missions-panel-body .panel { margin-bottom: -2px; }" +
+      "</style>").appendTo("head");
+})();
