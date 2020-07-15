e696d4bfae7f57ab1505bb477abbd1093b297ce7
MisteryMan <MisteryMan@users.noreply.github.com>
2020-07-15 05:31:12 +0200

    Update MM_missionList-cosmetics.user.js

--- MM_missionList-cosmetics.user.js
+++ MM_missionList-cosmetics.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         MM_missionList-cosmetics
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.missionchief.com/
