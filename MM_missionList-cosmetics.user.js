67e8c4e6c4677465e1c4c5f231b2df12d8631ec4
MisteryMan <MisteryMan@users.noreply.github.com>
2020-07-15 05:40:02 +0200

    Update MM_missionList-cosmetics.user.js

--- MM_missionList-cosmetics.user.js
+++ MM_missionList-cosmetics.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         MM_missionList-cosmetics
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.missionchief.com/
@@ -26,7 +26,7 @@
       "</style>").appendTo("head");
     $("#lss-layout-01").remove();
     $('<style rel="stylesheet" id="lss-layout-01" type="text/css">#map,#missions-panel-body{height:100%;}#map_outer{top:-19px;width:40%;height:calc(100vh - 90px);padding:0;}#buildings_outer,#chat_outer,#missions_outer,#radio_outer{width:calc(60% - 110px);height:calc(100vh - 90px);margin-left:20px;padding:0!important;float:left;overflow:hidden;overflow-y:hidden;}#buildings_outer,#chat_outer,#radio_outer{display:none;}#missions-panel-body{padding:0!important;}#buildings_outer .panel-body{height:calc(100vh - 135px)!important;padding-bottom:0;max-height:100%!important;}#chat_outer .panel-body,#radio_outer .panel-body{height:calc(100% - 40px)!important;max-height:100%!important;padding-bottom:0!important;}#chat_outer>div,#radio>div,#radio_outer>div{height:100%;margin:0;}#missions{height:100%;}#lssm_layout01_menu{width:45px;position:absolute;top:80px;right:17px;border-radius:5px;z-index:999;}#lssm_layout01_menu div{margin-bottom:20px;}.lssm_menu_btn{padding:10px;background-color:#fff;border-radius:4px;}.lssm_menu_btn:hover{background-color:#fb3c35;cursor:pointer;}.lssm_menu_btn:hover .lssm_menu_btn_svg {-webkit-filter:invert(100%) contrast(10);filter:invert(100%) contrast(10);cursor:pointer;}.lssm_menu_btn_active{padding:10px;background-color:#c9302c;border-radius:4px;}.lssm_menu_btn_active img,.lssm_menu_btn_svg:hover{-webkit-filter:invert(100%) contrast(10);filter:invert(100%) contrast(10);}.lssm_menu_btn_svg{width:20px;}#WachenplanungOnMap_settings{left:0!important;}#missions strong{display:none!important;}</style>').appendTo("head");
-	map.invalidateSize(true);
+	setTimeout(() => { map.invalidateSize(true); }, 1000);
 
     var original_func = missionMarkerAdd;
     moveProgressBar();
